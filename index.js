b09357c3c606f6dd4769c7afe1a425335716cc44
Yusif S <yusif.salam-zade@aalto.fi>
2018-08-30 22:14:15 +0300

    Exercise 3.2 done

--- index.js
+++ index.js
@@ -5,35 +5,40 @@ const app = express()
 let persons = [
     {
         name: "Arto Kutvas",
-        number: "040-123456", 
+        number: "040-123456",
         id: 1
     },
     {
         name: "Martti Tienari",
-        number: "040-123456", 
+        number: "040-123456",
         id: 2
     },
     {
         name: "Arto Jarvinen",
-        number: "040-123456", 
+        number: "040-123456",
         id: 3
     },
     {
         name: "Lea Kutvonen",
-        number: "040-123456", 
+        number: "040-123456",
         id: 4
     }
 ]
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) =>{
+app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p>
+    <p>${new Date()}</p>`)
+})
+
 const PORT = 3001
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
