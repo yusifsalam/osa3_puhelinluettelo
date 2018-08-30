2863cf1e3477fcf47ee66ed10401602412d7d7df
Yusif S <yusif.salam-zade@aalto.fi>
2018-08-30 22:21:35 +0300

    Exercise 3.3 done

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person){
+        res.json(person)
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p>
     <p>${new Date()}</p>`)
